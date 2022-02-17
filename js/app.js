e569425c4596c4ac5490e488059f522580e7768f
Abdul Samad Gomda <ag7766@nyu.edu>
2022-02-17 04:40:52 +0400

    Commit with relevant files

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const jokes = document.getElementById('dad-jokes');
+const jokesText = document.getElementById('joke-text');
+
+
+let gotJokes= false;
+
+
+
+
+// getting a dad joke
+async function getDadJoke(){
+    const response = await fetch("http://icanhazdadjoke.com", {
+        headers: {
+            Accept: "application/json",
+        },
+    });
+    const result = await response.json();
+    
+    const {joke} = result;
+    return joke;
+}
+
+async function displayJoke(){
+    //jokes.style.visibility="visible";
+    const text = await getDadJoke();
+    jokesText.innerHTML = text;
+}
+
+const hide = function hideJoke(){
+    if(gotJokes){
+        jokes.style.visibility="hidden";
+        gotJokes = false;
+    } else{
+        jokes.style.visibility="visible";
+        gotJokes = true;
+    }
+    
+}
+
+
+displayJoke();
+setInterval(function(){ displayJoke()}, 7000)
+
+//document.addEventListener('onClick', hide)
+
+window.addEventListener("click", () => {
+    hide();
+  });
